6bbce7444197595f8f341344f12f43a2560387df
Emre KAŞ <y.emrekas@outlook.com>
2020-06-29 02:47:36 +0300

    repository ve jwt configleri eklendi

--- src/poll/poll.module.ts
+++ src/poll/poll.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PollService } from './poll.service';
 import { PollController } from './poll.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PollRepository } from './poll.repository';
+import { UserModule } from 'user/user.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([PollRepository]),
+    UserModule
+  ],
   providers: [PollService],
   controllers: [PollController]
 })
-export class PollModule {}
+export class PollModule { }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { AuthService } from './services/auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt/jwt-strategy';
+import { UserRepository } from './user.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({
+      defaultStrategy: 'jwt'
+    }),
+    JwtModule.register({
+      secret: "veryverysecretkey",
+      signOptions: {
+        expiresIn: 36000
+      }
+    })
+  ],
   controllers: [UserController],
-  providers: [AuthService]
+  providers: [
+    AuthService,
+    JwtStrategy
+  ],
+  exports: [
+    JwtStrategy,
+    PassportModule
+  ]
 })
 export class UserModule {}
