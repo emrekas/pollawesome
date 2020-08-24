bd17a241030dcddbbab6f0638e27ee323d2b38ad
Emre <emre@edakik.com>
2020-08-24 18:00:31 +0300

    update typeorm config

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,9 +1,11 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-console.log(process.env.DATABASE_URL);
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  url: process.env.DATABASE_URL,
+  url:
+    process.env.DATABASE_URL ||
+    'postgres://dwcrryejufwbjh:f4e173ee04b117eca7ba0568760b5ada517b7960bf56e31c2624549d4132107d@ec2-54-247-118-139.eu-west-1.compute.amazonaws.com:5432/d7q18jkuuhqjst',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
+  ssl: process.env.DATABASE_URL ? true : false,
 };
