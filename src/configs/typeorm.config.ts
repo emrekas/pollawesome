3ec72d8d46486b271f9071c663ac5d35d90e1617
emrekas <y.emrekas@outlook.com>
2022-06-05 11:24:58 +0300

    changed db url

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -4,7 +4,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   url:
     process.env.DATABASE_URL ||
-    'postgres://dwcrryejufwbjh:f4e173ee04b117eca7ba0568760b5ada517b7960bf56e31c2624549d4132107d@ec2-54-247-118-139.eu-west-1.compute.amazonaws.com:5432/d7q18jkuuhqjst',
+    'postgres://kiqellacdxmhgy:c870ed5365573a7ff1658cb3cb6cba3a1c83660ab0ddd6915518cfdc57da259d@ec2-176-34-211-0.eu-west-1.compute.amazonaws.com:5432/d82sagksvj35hm',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
   extra: {
