e68ec0438c2af6c13b808f4ccc314073aeba23a1
emrekas <y.emrekas@outlook.com>
2022-06-11 01:44:24 +0300

    server deploy için değişiklikler yapıldı

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,14 @@ async function bootstrap() {
 
   const options = new DocumentBuilder().setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, options);
+
+  const port = process.env.YOUR_PORT || process.env.PORT || 3000;
+  const host = process.env.YOUR_HOST || '0.0.0.0';
+
   SwaggerModule.setup('api', app, document);
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(port, host, ()=>{
+    console.log('Listening on port %d', port);
+  });
 }
 bootstrap();
